5aa67d0a81ff177f9d2d9211e431bfed11e7b39e
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2024-05-06 01:21:04 +0100

    Simple cipher

--- caesar.py
+++ caesar.py
@@ -1,7 +1,13 @@
 import string
 
-plain_text = "Hello!"
-shift = 7
+plain_text = "Enter text to be encrypted here!"
+shift = 15
+shift %= 26
 
 alphabet = string.ascii_lowercase
 shifted = alphabet[shift:] + alphabet[:shift]
+table = str.maketrans(alphabet, shifted)
+
+encrypted = plain_text.translate(table)
+
+print(encrypted)
