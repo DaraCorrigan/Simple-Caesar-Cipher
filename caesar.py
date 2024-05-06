ef3cc1e899689c33d6f3f5e480966d6bde54e61b
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2024-05-06 01:29:40 +0100

    removed

--- caesar.py
+++ caesar.py
@@ -11,16 +11,3 @@ table = str.maketrans(alphabet, shifted)
 encrypted = plain_text.translate(table)
 
 print(encrypted)
-
-def caesar(text, shift, alphabets):
-
-    def shift_alphabet(alphabet):
-        return alphabet[shift:] + alphabet[:shift]
-
-    shifted_alphabets = tuple(map(shift_alphabet, alphabets))
-    final_alphabet = ''.join(alphabets)
-    final_shifted_alphabet = ''.join(shifted_alphabets)
-    table = str.maketrans(final_alphabet, final_shifted_alphabet)
-    return text.translate(table)
-
-plain_text = "Testing."
